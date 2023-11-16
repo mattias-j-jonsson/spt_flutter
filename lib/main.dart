732443b63c353d5c48f04c95e2c991c7df21ae14
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2023-11-16 17:22:06 +0100

    a cancellable countdown implemented

--- lib/main.dart
+++ lib/main.dart
@@ -56,24 +56,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final stopwatch = Stopwatch();
   String tooltipMessage = 'start';
-  double seconds = 0;
-  bool runs = false;
+  String timeString = '00:00';
+  int currentTick = 0;
+  int countdown = 5;
+  bool timerStatus = false;
+  Timer? timer;
 
-  // Timer.periodic(Duration(seconds: 3), (timer) {});
-
-  void _startStopClock() {
-    if (stopwatch.isRunning) {
-      setState(() {
-        stopwatch.stop();
-        tooltipMessage = 'start';
+  void _startTimer() {
+    if(!timerStatus){
+      tooltipMessage = 'stop';
+      timerStatus = true;
+      timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+        if(timer.tick < 6) {
+          setState(() {
+            countdown--;
+          });
+        } else {
+          tooltipMessage = 'start';
+          timerStatus = false;
+          countdown = 5;
+          timer?.cancel();
+          setState(() {
+        
+          });
+        }
       });
     } else {
+      tooltipMessage = 'start';
+      timerStatus = false;
+      countdown = 5;
+      timer?.cancel();
       setState(() {
-        stopwatch.start();
-        tooltipMessage = 'stop';
-        seconds = stopwatch.elapsedMilliseconds / 1000;
+        
       });
     }
   }
@@ -81,20 +96,18 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+      _counter++;
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // seconds = stopwatch.elapsedMilliseconds / 1000;
-    runs = stopwatch.isRunning;
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -131,19 +144,17 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Clock is running: $runs',
-              // 'You have pushed the button this many times:',
+              '$countdown'
             ),
             Text(
-              '$seconds',
-              // '$_counter',
+              timeString,
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _startStopClock,
+        onPressed: _startTimer,
         // onPressed: _incrementCounter,
         tooltip: tooltipMessage,
         // tooltip: 'Increment',
